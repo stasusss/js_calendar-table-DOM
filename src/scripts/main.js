d22d11483a66f60c57e10d1ac0415a169bceda68
Stanislav <stasusss07@gmail.com>
2020-06-16 16:55:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,57 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const daysCount = new Date(year, month, 0).getDate();
+  const weekStart = new Date(`${year}/${month}/1`).getDay();
+  const table = document.createElement('table');
+
+  element.append(table);
+  table.append(tHead(), tBody(daysCount, weekStart));
 }
+
+function tHead() {
+  const header = document.createElement('thead');
+
+  const daysList = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+  for (const day of daysList) {
+    const currentDay = document.createElement('th');
+
+    currentDay.textContent = day;
+    header.append(currentDay);
+  }
+
+  return header;
+};
+
+function tBody(daysCount, weekStart) {
+  const body = document.createElement('tbody');
+
+  const weeksQuantity = Math.ceil(daysCount / 7);
+
+  let mainCounter = 1;
+  let additionalCounter = 0;
+
+  for (let i = 0; i < weeksQuantity; i++) {
+    const row = document.createElement('tr');
+
+    body.append(row);
+
+    for (let j = 0; j < 7; j++) {
+      const cell = document.createElement('td');
+
+      if (mainCounter < weekStart) {
+        additionalCounter++;
+      } else if (mainCounter >= weekStart
+        && mainCounter <= daysCount + additionalCounter) {
+        cell.textContent = mainCounter - additionalCounter;
+      }
+      mainCounter++;
+      row.append(cell);
+    }
+  }
+
+  return body;
+};
 
 calendarTable(2019, 10, calendar);
